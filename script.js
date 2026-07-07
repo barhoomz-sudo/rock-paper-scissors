2987a58a01e8f75922a1cba251feddba7f56554f
barhoomz <ibrahim.hu098@gmail.com>
2026-07-07 14:08:09 +0300

    added human choice functionality

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 function getComputerChoice(){
     // generates a random number from 1 to 3
-    let option = Math.floor(Math.random() * 3) + 1
+    let computerChoice = Math.floor(Math.random() * 3) + 1
     // console.log(option);
 
-    if(option === 1){
+    if(computerChoice === 1){
         return 'rock'
-    }else if(option === 2){
+    }else if(computerChoice === 2){
         return 'paper'
     }else{
         return 'scissors'
     }
 }
+
+function getHumanChoice(){
+    let humanChoice = prompt("Enter your move.").toLowerCase();
+    // console.log(humanChoice);
+    
+    if(humanChoice === "rock"){
+        return 'rock'
+    }else if(humanChoice === "paper"){
+        return 'paper'
+    }else if(humanChoice === "scissors"){
+        return 'scissors'
+    }else{
+        console.log("plz try again!");
+    }
+}
+
+
